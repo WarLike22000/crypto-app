b103ca05006b000fc6fdf8ac551fd32288de49ad
Alireza Kamali <kamalialireza788@gmail.com>
2023-05-27 16:01:02 +0330

    problem netlify

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 //context
 import { cryptoContext } from '../context/TopListContextProvider.jsx';
 //styles
--- src/components/shared/Coin.jsx
+++ src/components/shared/Coin.jsx
@@ -1,4 +1,3 @@
-import { Button } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 //styles
